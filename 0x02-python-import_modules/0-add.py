075381ea067eff2d6423d83ef40196fa34c35c6b
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-02 04:51:58 +0000

    updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add #import add function
+from add_0 import add  # import add function
 
 a = 1
 b = 2
 
+# prevent other modules from running this piece of code
+# run only when this module is called by the interpreter
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
