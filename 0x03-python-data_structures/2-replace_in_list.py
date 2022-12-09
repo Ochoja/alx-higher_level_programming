3b388a944de69037ee1c6830a8a046637e14af2e
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-09 07:22:19 +0000

    updated

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    """my_list takes list argument, idx
+       refers holds index value, element
+       value replaces value at index """
+
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list) - 1:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
