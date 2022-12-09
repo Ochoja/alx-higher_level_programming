3d846c42b164bd963c1700cf872bd2fff1ca1ce3
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-09 17:25:40 +0000

    updated

--- 0x03-python-data_structures/1-main.py
+++ 0x03-python-data_structures/1-main.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-matrix = [
-    [3,4,3],
-    [35,5,1]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
+my_list = [1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -8,4 +8,6 @@ def print_reversed_list_integer(my_list=[]):
         length = (len(my_list) * -1) - 1
 
         for i in range(-1, length, -1):
-            print("{:d}".format(my_list[i]), end="\n")
+            print("{:d}".format(my_list[i]))
+    else:
+        print("")
