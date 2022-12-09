002d7af0cec684811d41aa06bd1dbfac12e1d605
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-09 13:49:59 +0000

    updated

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -8,7 +8,7 @@ def no_c(my_string):
     for i in my_string:
         if i == 'c' or i == 'C':
             my_string = my_string[:count] + my_string[count+1:]
-            count -= 1
+            count -= 1 # reduce size of count as size of string reduces
         count += 1
 
     return my_string
