130c05be1b75ab009059ac574eb8627750ea4d79
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-09 16:55:46 +0000

    updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,11 @@ def print_matrix_integer(matrix=[[]]):
     print matrix of integers
     """
     for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j == len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]))
-            else:
-                print("{:d}".format(matrix[i][j]), end=" ")
+        if type(matrix[i]) == list:
+            for j in range(len(matrix[i])):
+                if j == len(matrix[i]) - 1:
+                    print("{:d}".format(matrix[i][j]))
+                else:
+                    print("{:d}".format(matrix[i][j]), end=" ")
+        else:
+            print("{:d}".format(matrix[i]))
