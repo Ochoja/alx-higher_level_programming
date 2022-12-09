fd15f8d604aa79d53a5000e5877404b990bd9adf
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-09 19:03:42 +0000

    updated

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,19 +6,23 @@ def add_tuple(tuple_a=(), tuple_b=()):
     element 1 in tuple_b
     """
     if tuple_a == ():
-        return tuple_b
+        return tuple_b[:2]
     elif tuple_b == ():
-        return tuple_a
+        return tuple_a[:2]
 
     new_tuple = ()
 
     if len(tuple_a) > len(tuple_b):
         for i in range(len(tuple_b)):
+            if i == 2:
+                break
             temp = (tuple_a[i] + tuple_b[i],)
             new_tuple += temp
-        new_tuple += tuple_a[i+1:]
     else:
         for i in range(len(tuple_a)):
+            if i == 2:
+                break
             temp = (tuple_a[i] + tuple_b[i],)
             new_tuple += temp
+
     return new_tuple
