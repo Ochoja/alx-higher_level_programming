92d5f05983c332119882beb1c67f63aa8459e6ae
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-14 17:07:27 +0000

    updated

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -7,8 +7,8 @@ def search_replace(my_list, search, replace):
     @replace is the new element
     """
     new_list = my_list.copy()
-    for i in new_list:
-        if search == i:
-            i = replace
+    for i in range(len(new_list)):
+        if search == new_list[i]:
+            new_list[i] = replace
 
     return new_list
