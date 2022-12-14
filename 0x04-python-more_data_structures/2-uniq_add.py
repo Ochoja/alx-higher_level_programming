682ccc4a615737e211d01a5bc4346e248c097b1c
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-14 17:28:36 +0000

    added 2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    """
+    adds all unique integers in a list
+    """
+    new_set = set(my_list)
+    total = 0
+
+    for i in new_set:
+       total += i
+
+    return total
