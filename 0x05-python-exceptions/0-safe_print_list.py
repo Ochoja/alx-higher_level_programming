3c7801194f44a8531eaf043cec6201e605ca3504
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-20 03:40:15 +0000

    fixed bugs

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,5 @@ def safe_print_list(my_list=[], x=0):
             num_printed += 1
         except IndexError:
             break
-        finally:
-            print("\n")
-
+    print()
     return num_printed
