6d5864477b254bf483a86aa0ad909c0df9dc2b98
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-20 04:31:22 +0000

    added 4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    divides element by element 2 lists
+    """
+    new_list = []
+
+    for i in range(list_length):
+        try:
+            new_list.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            new_list.append(0)
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+            if my_list_1[i] or my_list_2[i]:
+                new_list.append(0)
+            else:
+                break
+        finally:
+            return new_list
