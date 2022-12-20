6664b46eb54d308be2390a999de7245bb92f0040
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-20 03:37:14 +0000

    added finally to task 0 file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,5 +11,7 @@ def safe_print_list(my_list=[], x=0):
             num_printed += 1
         except IndexError:
             break
+        finally:
+            print("\n")
 
     return num_printed
