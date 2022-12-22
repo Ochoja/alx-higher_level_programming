2151a17ffc82e1b98c73dc1af64d4a3b0bb6f95d
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-22 22:22:24 +0000

    updated

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,14 +3,16 @@ def best_score(a_dictionary):
     """
     returns a key with the biggest integer value
     """
-    if a_dictionary == {}:
-        return None
-    else:
+    if a_dictionary:
         first_key = list(a_dictionary.keys())[0]
         biggest = a_dictionary[first_key]
+        big_key = first_key
 
         for key in a_dictionary:
             if a_dictionary[key] >= biggest:
                 biggest = a_dictionary[key]
+                big_key = key
 
-        return biggest
+        return big_key
+    else:
+        return None
