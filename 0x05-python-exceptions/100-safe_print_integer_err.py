fec47aa62e695256dbef071df78309275c641f55
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-23 18:03:14 +0000

    updated

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import sys
+
 def safe_print_integer_err(value):
     """
     function prints an integer
@@ -7,5 +9,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Exception: {}".format(e))
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
