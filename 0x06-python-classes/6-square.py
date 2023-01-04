1212bf59eb786b59dd37ec315a355f1e1da87d8f
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-04 16:06:14 +0000

    updated

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,8 +8,8 @@ class Square:
     """class defines a square
     """
     def __init__(self, size=0, position=(0, 0)):
-        self.size = size
-        self.position = position
+        self.__size = size
+        self.__position = position
 
     def area(self):
         """
