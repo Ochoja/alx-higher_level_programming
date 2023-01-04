5e1780797cf0c9d02fe7ebd073cc89597be45f74
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-04 02:37:20 +0000

    updated

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Contains Square class
+"""
+
+
+class Square:
+    """
+    defines square
+    """
+
+    def __init__(self, size):
+        """
+        instantiates square size
+        """
+        self.__size = size
