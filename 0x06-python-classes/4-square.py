243df2952df389de964b1e3cc3c303bc4f75500f
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-04 03:08:48 +0000

    updated

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+contains square class
+"""
+
+
+class Square:
+    """
+    class defines a square
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        """
+        returns the area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size getter"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
