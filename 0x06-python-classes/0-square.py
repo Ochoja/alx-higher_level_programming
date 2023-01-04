3b2f5d09b4bb3c65730331da81c23ab8836ddde9
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-04 02:30:52 +0000

    updated

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""
+Module contains Square Class
+"""
+
 class Square:
-    """Square is an empty class which
+    """
+    Square is an empty class which
     defines a square
     """
     pass
