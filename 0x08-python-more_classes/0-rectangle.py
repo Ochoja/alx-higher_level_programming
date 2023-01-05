60fc6c24fea80b1aaea2ce48855f8fedbe6ce9c2
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-05 02:41:57 +0000

    more classes task 0 added

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Contains Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,4 @@
+# More Classes and Objects
+## Areas Covered
+1. Class and Static Methods
+2. __str__ and __repr__
