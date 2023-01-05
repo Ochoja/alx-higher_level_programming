0814a3e8ffa40b5501d59a9b396eb69b900ca319
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-05 02:56:23 +0000

    updated

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """Rectangle class"""
     def __init__(self, width=0, height=0):
+        """instantiation method"""
         self.width = width
         self.height = height
 
