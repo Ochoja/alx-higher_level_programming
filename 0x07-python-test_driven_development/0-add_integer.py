b3ad2b9abd982fff5c3317819bd50e3b9c470e29
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-09 02:57:10 +0000

    test-driven-development in python

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module contains add_integer function
+
+add_integer:
+Function adds two integers and returns the value
+"""
+
+
+def add_integer(a, b=98):
+    """adds two integer
+    Args: a, b
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    a = round(a)
+    b = round(b)
+    return a + b
