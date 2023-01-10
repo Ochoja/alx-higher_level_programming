32e46c0211408b104f860310a48bf03583697ac9
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-10 02:51:33 +0000

    updated

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+contains lookup function
+"""
+
+def lookup(obj):
+    """
+    Returns a list of available attributes
+    and methods of an object
+    """
+    return obj.__dict__
