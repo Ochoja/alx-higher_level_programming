e54fb4f4108c2435049c4942c941d8cd1a9127a1
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-10 11:52:49 +0000

    updated

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -19,12 +19,13 @@ class BaseGeometry:
         """
         validates value
         """
-        if not isinstance(value, int):
+        if not isinstance(value, int) or type(value) == bool:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
         if not isinstance(name, str):
             raise TypeError("name must be a string")
+        return True
 
 
 class Rectangle(BaseGeometry):
@@ -32,7 +33,7 @@ class Rectangle(BaseGeometry):
     Rectangle class that inherits from BaseGeometry
     """
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        if self.integer_validator("width", width):
+            self.__width = width
+        if self.integer_validator("height", height):
+            self.__height = height
