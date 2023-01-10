2f447c06a7dfe5369665914e34623970b7213192
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-10 17:05:44 +0000

    updated

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,12 +4,31 @@ contains Rectangle class which inherits
 from BaseGeometry
 """
 
+class BaseGeometry:
+    """
+    Base geometry class
+    """
+    def area(self):
+        """
+        raises an exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates value
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        if not isinstance(name, str):
+            raise TypeError("name must be a string")
+
+
 class Rectangle(BaseGeometry):
     """
     Rectangle class inherits from BaseGeometry
-
-
-
     """
     def __init__(self, width, height):
         self.integer_validator("width", width)
