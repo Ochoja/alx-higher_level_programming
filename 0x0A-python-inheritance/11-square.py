04a8193c1fbbfe898224a825117923102175797d
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-10 18:14:41 +0000

    updated

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,45 +5,7 @@ Square class that inherits from Rectangle
 """
 
 
-class BaseGeometry:
-    """
-    Base geometry class
-    """
-    def area(self):
-        """
-        raises an exception
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        validates value
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        if not isinstance(name, str):
-            raise TypeError("name must be a string")
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
-    def __init__(self, width, height):
-        """ Constructor """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """returns area of rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """runs when print() or str() is called on instance"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Square class that inherits from Rectangle"""
