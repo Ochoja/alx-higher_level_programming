1a048e9a61306b8ce1d278de66bb694261ed0700
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-10 16:58:11 +0000

    updated

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -17,11 +17,10 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__height = height
 
-
     def area(self):
         """returns area of rectangle"""
         return self.__width * self.__height
 
-
     def __str__(self):
+        """str() method return"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
