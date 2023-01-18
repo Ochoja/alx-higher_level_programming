964efbcdb26b653ff0be4daf87f213d70bbecc25
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-18 16:22:21 +0000

    updated

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,8 +1,8 @@
 import unittest
-from models.base import Base
 import sys
-
 sys.path.append('../../')
+from models.base import Base
+
 
 class TestBaseClass(unittest.TestCase):
     def test_Base(self):
@@ -16,3 +16,7 @@ class TestBaseClass(unittest.TestCase):
         self.assertEqual(d.id, 12)
         e = Base()
         self.assertEqual(e.id, 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
