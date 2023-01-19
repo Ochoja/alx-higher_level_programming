fc6171b9debd9415bad5064ba35094169eb07a14
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-19 14:39:16 +0000

    added square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Square inherits from Rectangle"""
+
+
+class Square(Rectangle):
+    """Square class based on Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
