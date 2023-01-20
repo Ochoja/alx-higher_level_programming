2d204f5df71ec35a90f6bf4a89e1e3e8ec07985f
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-20 12:50:33 +0000

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import sys
 """Read a file"""
 
 
 def read_file(filename=""):
     """Reads a file"""
     with open(filename, mode='r', encoding="utf-8") as text_file:
-        print(text_file.readlines(), file=sys.stdout)
+        print(text_file.read(), end="")
