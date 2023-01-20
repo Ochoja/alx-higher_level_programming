89a5b4e2609383c1c302edd46bc51ca9ccdd6d76
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-20 03:51:48 +0000

    updated

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,19 +8,18 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Constructor"""
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
-        self.height = self.width = value
-        self.__size = value
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
         """updated arguments"""
