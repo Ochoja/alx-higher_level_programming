651e2406ec80325b33582068c46575931d11c4a2
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-20 04:42:15 +0000

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,7 @@
 
 def read_file(filename=""):
     """Reads a file"""
-    with open(filename, encoding="UTF8", mode='r') as text_file:
-        text_file.read()
+    with open(filename, encoding="UTF8") as text_file:
+        print(text_file.read())
+
+read_file("my_file_0.txt")
