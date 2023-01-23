fead597908fcc0455dafb594419e1713067f1ab4
Daniel Ochoja <danielochoja@yahoo.com>
2023-01-23 13:45:53 +0000

    updated

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""add all argument to a python list
+and save them to a file"""
 import sys
+
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
