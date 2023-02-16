139249128aee30dbe2694cde6ea5b6d1e473cd01
Daniel Ochoja <danielochoja@yahoo.com>
2023-02-16 16:45:06 +0000

    updated

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- List records
 -- List records in ascending order based on score
-SELECT score, name FROM second_table WHERE name != 'NULL' ORDER BY score;
+SELECT score, name FROM second_table WHERE name != 'NULL' ORDER BY score DESC;
