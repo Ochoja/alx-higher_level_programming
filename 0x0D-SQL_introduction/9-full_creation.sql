492989df886e8b931a7786a27759e7dbd4fe7376
Daniel Ochoja <danielochoja@yahoo.com>
2023-02-16 15:19:29 +0000

    Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Creates a table in a database
+-- Creates a table `second_table`
+CREATE TABLE IF NOT EXISTS second_table
+(id INT, name VARCHAR(256), score INT);
+
+--insert record
+INSERT INTO second_table VALUES(1, "John", 3);
+-- insert record
+INSERT INTO second_table VALUES(2, "Alex", 3);
+-- insert record
+INSERT INTO second_table VALUES(3, "Bob", 14);
+-- insert record
+INSERT INTO second_table VALUES(4, "George", 8);
