c35d27c47cc53b30fcdaaf8e730679fbddacaf3c
Daniel Ochoja <danielochoja@yahoo.com>
2023-02-16 16:42:49 +0000

    updated

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- List number of records with same score
+-- Display score and number of records for the score
+SELECT score COUNT(score) 
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- List records
+-- List records in ascending order based on score
+SELECT score, name FROM second_table WHERE score != 'NULL' ORDER BY score;
