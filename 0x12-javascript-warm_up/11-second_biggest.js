0f083deda5283cca169808a5c63e813ab466b2bb
Daniel Ochoja <danielochoja@yahoo.com>
2023-03-14 01:54:47 +0000

    updated

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 /* Searches for second biggest number */
+function compareNumbers (a, b) {
+  return a - b;
+}
+
 if (process.argv[2]) {
   const list = process.argv.slice(2);
 
   if (list.length <= 1) {
     console.log(0);
   } else {
-    list.sort();
+    list.sort(compareNumbers);
     console.log(list[list.length - 2]);
   }
 } else {
