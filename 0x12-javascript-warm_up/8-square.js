8286b166b82bb2079098b18e18408c8bda99eb9e
Daniel Ochoja <danielochoja@yahoo.com>
2023-03-14 01:11:35 +0000

    updated

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* Script that prints a square */
+if (process.argv[2]) {
+  let text = '';
+  for (let i = 0; i < process.argv[2]; i++) {
+    text += 'X';
+  }
+
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log(text);
+  }
+} else {
+  console.log('Missing size');
+}
