ce3b0bf20dfb1640328ba68dff7a49fd82722baf
Daniel Ochoja <danielochoja@yahoo.com>
2023-03-14 01:13:11 +0000

    updated script

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* Script that prints a square */
-if (process.argv[2]) {
+if (!isNaN(process.argv[2])) {
   let text = '';
   for (let i = 0; i < process.argv[2]; i++) {
     text += 'X';
