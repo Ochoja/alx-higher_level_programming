48703d5ea818ea4ea4e70bdb03633d8d735da2eb
Daniel Ochoja <danielochoja@yahoo.com>
2023-03-20 05:32:06 +0000

    updated

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
 
     # perform db query using cursor
     cursor = db.cursor()
-    cursor.execute("""SELECT id, name FROM states WHERE
-                   name='{}' ORDER BY id ASC;""".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE \
+                   name='{}' ORDER BY id".format(sys.argv[4]))
 
     # get query result
     result = cursor.fetchall()
