e23e2d2dd25818dfb2e017626152b2a26074946c
Daniel Ochoja <danielochoja@yahoo.com>
2023-03-21 02:02:56 +0000

    updated

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Lists cities from the database hbtn_0e_4_usa
+refer to './4-cities_by_state.sql' """
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    # create connection to database
+    db = MySQLdb.connect(host="localhost", user=f"{sys.argv[1]}",
+                         password=f"{sys.argv[2]}", database=f"{sys.argv[3]}")
+
+    # perform db query using cursor
+    cursor = db.cursor()
+    cursor.execute("SELECT id, cities.name, name FROM states \
+                    JOIN cities ON states.id=cities.state_id")
+
+    # get and print results
+    result = cursor.fetchall()
+
+    for i in result:
+        print(i)
+
+    # clean up
+    cursor.close()
+    db.close()
