0eae01084c6c425eed6c199300c0e2ca1f7fa8a2
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-04 09:57:42 +0000

    updated

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,3 +9,22 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, 3]), 3)
         self.assertEqual(max_integer([2, 3.5, 8.9]), 8.9)
         self.assertEqual(max_integer([2, 5.2, 9.1, 3.33]), 9.1)
+
+    def test_tuple(self):
+        self.assertEqual(max_integer((1, 2, 3)), 3)
+        self.assertEqual(max_integer((1.0, 7.9, 5.6)), 7.9)
+
+    def test_empty(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_string(self):
+        self.assertEqual(max_integer("Strong"), 't')
+
+    def test_dictionary(self):
+        self.assertRaises(KeyError, max_integer, {"key": "one", "two": "two"})
+
+    def test_set(self):
+        self.assertRaises(TypeError, max_integer, {"key", "value"})
+
+    def test_array(self):
+        self.assertRaises(TypeError, max_integer, [2, 3, "value"])
