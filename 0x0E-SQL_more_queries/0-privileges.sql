a2630c2f6ef4911ad231aef9982bf4e97b60c40d
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-10 04:22:32 +0000

    updated

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Lists privileges of users
+-- Lists all privileges of user `user_0d_1`
+SHOW GRANTS FOR user_0d_1@localhost;
+-- List all privileges of user `user_0d_2`
+SHOW GRANTS FOR user_0d_2@localhost;
