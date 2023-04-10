b8dd0316ee61cc1cddfa66b700f190a8289a1f96
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-10 06:06:14 +0000

    updated

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Creates a database and a table in the database
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Select database
+USE hbtn_0d_usa;
+-- Create table in database
+CREATE TABLE IF NOT EXISTS cities (
+id INT NOT NULL AUTO_INCREMENT,
+state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY (id)
+);
