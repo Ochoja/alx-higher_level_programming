57c423bc2199af8a1910ea550db87ade6c9b4481
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-10 06:10:29 +0000

    updated

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,7 +6,8 @@ USE hbtn_0d_usa;
 -- Create table in database
 CREATE TABLE IF NOT EXISTS cities (
 id INT NOT NULL AUTO_INCREMENT,
-state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
-PRIMARY KEY (id)
+PRIMARY KEY (id),
+FOREIGN KEY (state_id) REFERENCES states(id)
 );
