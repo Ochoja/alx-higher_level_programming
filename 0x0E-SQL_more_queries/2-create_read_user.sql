46588e73951d2394e1f4c072816e1227570f465f
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-10 04:59:29 +0000

    updated

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Create a database and user and grant SELECT privilege to user
 -- Create database hbtn_0d_2
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create user user_0d_2
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 -- Set password of user user_0d_2
