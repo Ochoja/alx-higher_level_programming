1fe0ba1048f736f9000816e91420ad022f48fe33
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-11 02:43:49 +0000

    updated

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
 -- List rows in a database
 -- List cities with state_id = 1
 SELECT id, name FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name='California')
-SORT BY id ASC;
+WHERE state_id = (SELECT id FROM states WHERE name='California');
