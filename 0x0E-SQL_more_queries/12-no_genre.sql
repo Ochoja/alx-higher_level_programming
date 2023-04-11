d6a525212942fc9c61a2d405eaea0da52902ada4
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-11 03:34:02 +0000

    updated

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Select records with no genre
+-- Left join without intersection
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
