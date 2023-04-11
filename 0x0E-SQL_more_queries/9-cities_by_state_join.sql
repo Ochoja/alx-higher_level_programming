b8c8420ae81dbb0d0121bda4efa22b480072c059
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-11 02:58:02 +0000

    updated

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,5 @@
 -- Select cities and state
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.state_id=state.id;
+JOIN states ON cities.state_id=state.id
+ORDER BY cities.id ASC;
