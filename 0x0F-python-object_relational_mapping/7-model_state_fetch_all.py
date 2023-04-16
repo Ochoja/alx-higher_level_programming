ee115a6f86872dd481fcaee171ba655958285435
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-16 15:38:49 +0000

    updated

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,14 +7,14 @@ from sqlalchemy.orm import Session  # used for CRUD operations
 
 
 if __name__ == '__main__':
-    url = f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
-    engine = create_engine(url, echo=True)
+   url = f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
+    engine = create_engine(url, pool_pre_ping=True)
 
     # Create table
     Base.metadata.create_all(engine)  # Connect model to database engine
     session = Session(engine)  # session is used for CRUD operations
 
-    for instance in session.query(State).order_by(State.id):
+    for instance in session.query(State).order_by(State.id).all():
         print(f"{instance.id}: {instance.name}")
 
     session.close()
