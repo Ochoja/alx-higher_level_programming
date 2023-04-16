3047c465cf2e696be7c36b035e3e15b8232ef84e
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-16 15:39:47 +0000

    updated

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""List information in a table using sqlalchemy"""
-import sys
-from model_state import Base, State  # contains table definition
-from sqlalchemy import create_engine  # used to Connect to database
-from sqlalchemy.orm import Session  # used for CRUD operations
+"""script that lists all State objects from the database hbtn_0e_6_usa
+   with sqlchemy
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
 
 
-if __name__ == '__main__':
-   url = f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}"
-    engine = create_engine(url, pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    # Create table
-    Base.metadata.create_all(engine)  # Connect model to database engine
-    session = Session(engine)  # session is used for CRUD operations
+    Base.metadata.create_all(engine)
+    session = Session(engine)
 
-    for instance in session.query(State).order_by(State.id).all():
-        print(f"{instance.id}: {instance.name}")
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
