e3998405fa9aadf5af38cfd65b707468a3246cf6
Daniel Ochoja <37149152+Ochoja@users.noreply.github.com>
2023-04-19 03:26:18 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-/** Evaluates and print argument if its an integer**/
-if (isNaN(parseInt(process.argv[2]))) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${process.argv[2]}`);
-}
