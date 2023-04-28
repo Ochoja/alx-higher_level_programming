67218a9b131871cfd50e8d61e1118df75bfd0450
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-28 01:17:37 +0000

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
-# A script that sends a request and display the size of the response body
+#!/bin/bash
+# A script that display the size of the response body
 curl -s -w '%{size_download}\n' -o /dev/null "$1"
