233658e4009954cfa7c6530a8d1e579754a5e892
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-28 03:13:43 +0000

    updated

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,8 +6,9 @@ from model_state import Base, State
 from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://argv[1]:argv[2]//@localhost/argv[3]')
-    
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}
+    :{argv[2]}//@localhost/{argv[3]}')
+
     session = Session(engine)
 
     states = session.query(State).order_by(State.id).\
