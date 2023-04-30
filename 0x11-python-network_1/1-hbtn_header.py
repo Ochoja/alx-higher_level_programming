ba97cd3fad01adb56b364676f63828c6310d94e4
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-30 06:13:44 +0000

    updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,11 +3,11 @@
 displays the value of X-Request-Id"""
 import urllib.request, sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        for x in str(response.info()).splitlines():
-            if 'X-Request-Id' in x:
-                request_id = x
-        print(request_id.split()[1])
+url = sys.argv[1]
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    for x in str(response.info()).splitlines():
+        if 'X-Request-Id' in x:
+            request_id = x
+            break
+    print(request_id.split()[1])
