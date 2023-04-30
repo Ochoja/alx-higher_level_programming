62dbb8c12e7283f4e1afe32867a68caae113f0cc
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-30 13:56:29 +0000

    updated

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import urllib.parse
+import urllib.request
+from sys import argv
+
+
+url = argv[1]
+values = {'email': f"{argv[2]}"}
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+
+with urllib.request.urlopen(req) as response:
+    page = str(response.read())
+    print(page[2:len(page) - 1])
