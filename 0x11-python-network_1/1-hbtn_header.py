d951ab459d971fa809b5879577258fd8005f4d33
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-30 06:19:30 +0000

    updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """Script that sends request to URL and
 displays the value of X-Request-Id"""
-import urllib.request, sys
+import urllib.request
+from sys import argv
 
-url = sys.argv[1]
+url = argv[1]
 req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    for x in str(response.info()).splitlines():
-        if 'X-Request-Id' in x:
-            request_id = x
-            break
-    print(request_id.split()[1])
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(req) as response:
+        for x in str(response.info()).splitlines():
+            if 'X-Request-Id' in x:
+                request_id = x
+                break
+        print(request_id.split()[1])
