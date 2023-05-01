c874edc6a4303e51a2ec89f58fca24cea8b78d98
Daniel Ochoja <danielochoja@yahoo.com>
2023-05-01 11:55:18 +0000

    updated

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,9 +4,9 @@ to the passed URL with email as paramenter"""
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    response = requests.post(url, data={'email': email})
 
-url = argv[1]
-email = argv[2]
-response = requests.post(url, data={'email': email})
-
-print(response.text)
+    print(response.text)
