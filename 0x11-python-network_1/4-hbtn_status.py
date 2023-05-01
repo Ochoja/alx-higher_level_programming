32bc10ed6a36569249633515b2fcc5f425ca4730
Daniel Ochoja <danielochoja@yahoo.com>
2023-05-01 11:27:49 +0000

    updated

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Fetches `https://alx-intranet.hbtn.io/status`"""
+import requests
+
+
+response = requests.get('https://alx-intranet.hbtn.io/status')
+print("Body response:")
+print(f"\t- type: {type(response.text)}")
+print(f"\t- content: {response.text}")
